caa2dfa3cf9058be1bbb933d00069242dea3e70b
shivam <shivamgoel150@gmail.com>
2022-10-03 23:52:23 +0530

    signup activity design complete

--- app/src/main/java/com/example/projectmanagementapp/IntroActivity.kt
+++ app/src/main/java/com/example/projectmanagementapp/IntroActivity.kt
@@ -1,11 +1,18 @@
 package com.example.projectmanagementapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_intro.*
 
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
+
+        btn_sign_in_intro.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/projectmanagementapp/SplashScreenActivity.kt
+++ app/src/main/java/com/example/projectmanagementapp/SplashScreenActivity.kt
@@ -6,16 +6,16 @@ import android.os.Bundle
 import android.os.Handler
 
 class SplashScreenActivity : AppCompatActivity() {
-    lateinit var handler:Handler
+    lateinit var handler: Handler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        handler=Handler()
+        handler = Handler()
         handler.postDelayed({
-            val intent =Intent(this, IntroActivity::class.java)
+            val intent = Intent(this, IntroActivity::class.java)
             startActivity(intent)
             finish()
-        },3000)
+        }, 3000)
     }
 }
