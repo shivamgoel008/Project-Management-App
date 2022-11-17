3a2a6fb6762cc59118290aa936271939a43cbf08
shivam <shivamgoel150@gmail.com>
2022-11-17 18:32:15 +0530

    Loading Card Details To Set The Title Of The Card

--- app/src/main/java/com/example/projectmanagementapp/CardDetailsActivity.kt
+++ app/src/main/java/com/example/projectmanagementapp/CardDetailsActivity.kt
@@ -1,12 +1,48 @@
 package com.example.projectmanagementapp
 
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_card_details.*
 
 
 class CardDetailsActivity : BaseActivity() {
+
+    private lateinit var mBoardDetails: Board
+    private var mTaskListPosition: Int = -1
+    private var mCardPosition: Int = -1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_card_details)
 
+        getIntentData()
+        setupActionBar()
+
+    }
+
+    private fun setupActionBar() {
+
+        setSupportActionBar(toolbar_card_details_activity)
+
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_white_color_back_24dp)
+            actionBar.title = mBoardDetails.taskList[mTaskListPosition].cards[mCardPosition].name
+        }
+
+        toolbar_card_details_activity.setNavigationOnClickListener { onBackPressed() }
+    }
+
+    private fun getIntentData() {
+
+        if (intent.hasExtra(Constants.BOARD_DETAIL)) {
+            mBoardDetails = intent.getParcelableExtra(Constants.BOARD_DETAIL)!!
+        }
+        if (intent.hasExtra(Constants.TASK_LIST_ITEM_POSITION)) {
+            mTaskListPosition = intent.getIntExtra(Constants.TASK_LIST_ITEM_POSITION, -1)
+        }
+        if (intent.hasExtra(Constants.CARD_LIST_ITEM_POSITION)) {
+            mCardPosition = intent.getIntExtra(Constants.CARD_LIST_ITEM_POSITION, -1)
+        }
     }
 }
